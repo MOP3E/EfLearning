0cd26121191f4abb78a66709ceb3f1150079020d
MOP3E <mop3e@mail.ru>
2024-06-03 17:00:48 +0300

    Добавьте файлы проекта.

---
+++ EFGetStarted/Model.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFGetStarted
+{
+    /// <summary>
+    /// ???
+    /// </summary>
+    public class BloggingContext : DbContext
+    {
+        /// <summary>
+        /// Таблица с блогами.
+        /// </summary>
+        public DbSet<Blog> Blogs { get; set; }
+
+        /// <summary>
+        /// Таблица с сообщениями.
+        /// </summary>
+        public DbSet<Post> Posts { get; set; }
+
+        /// <summary>
+        /// Файл БД SQLITE.
+        /// </summary>
+        public string DbFile { get; }
+
+        public BloggingContext()
+        {
+            //настройка полного имени файла БД SQLITE
+            DbFile = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "blogging.db");
+        }
+
+        //public BloggingContext(DbContextOptions<BloggingContext> options) : base(options)
+        //{
+        //}
+
+        //создать файл БД SQLITE в папке локальных данных программ
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbFile}");
+    }
+
+    /// <summary>
+    /// Модель таблицы с блогами.
+    /// </summary>
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    /// <summary>
+    /// Модель таблицы с сообщениями.
+    /// </summary>
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFGetStarted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dbFile = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "blogging.db");
+
+            DbContextOptions<BloggingContext> contextOptions = new DbContextOptionsBuilder<BloggingContext>()
+                .UseSqlite($"Data Source={dbFile}")
+                .Options;
+
+            //объект для доступа к базе данных
+            using BloggingContext db = new(contextOptions);
+
+            //ВАЖНО: в этом примере ожидается, что база данных уже создана и существует
+            Console.WriteLine($"Путь к базе данных: {dbFile}.");
+
+            //создание содержимого базы данных
+            db.Database.ExecuteSqlRaw(
+                "CREATE TABLE IF NOT EXISTS \"__EFMigrationsHistory\" (\r\n    \"MigrationId\" TEXT NOT NULL CONSTRAINT \"PK___EFMigrationsHistory\" PRIMARY KEY,\r\n    \"ProductVersion\" TEXT NOT NULL\r\n);\r\n\r\nBEGIN TRANSACTION;\r\n\r\nCREATE TABLE \"Blogs\" (\r\n    \"BlogId\" INTEGER NOT NULL CONSTRAINT \"PK_Blogs\" PRIMARY KEY AUTOINCREMENT,\r\n    \"Url\" TEXT NULL\r\n);\r\n\r\nCREATE TABLE \"Posts\" (\r\n    \"PostId\" INTEGER NOT NULL CONSTRAINT \"PK_Posts\" PRIMARY KEY AUTOINCREMENT,\r\n    \"Title\" TEXT NULL,\r\n    \"Content\" TEXT NULL,\r\n    \"BlogId\" INTEGER NOT NULL,\r\n    CONSTRAINT \"FK_Posts_Blogs_BlogId\" FOREIGN KEY (\"BlogId\") REFERENCES \"Blogs\" (\"BlogId\") ON DELETE CASCADE\r\n);\r\n\r\nCREATE INDEX \"IX_Posts_BlogId\" ON \"Posts\" (\"BlogId\");\r\n\r\nINSERT INTO \"__EFMigrationsHistory\" (\"MigrationId\", \"ProductVersion\")\r\nVALUES ('20240603105431_InitialCreate', '8.0.6');\r\n\r\nCOMMIT;\r\n\r\n"
+            );
+
+            //создание нового блога
+            Console.WriteLine("Создание нового блога");
+            db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+            db.SaveChanges();
+
+            //чтение блога
+            Console.WriteLine("Запрос последнего созданного блога");
+            Blog blog = db.Blogs
+                .OrderByDescending(b => b.BlogId)
+                .First();
+
+            //обновление блога и добавление сообщения
+            Console.WriteLine("Обновление блога и добавление сообщения");
+            blog.Url = "https://devblogs.microsoft.com/dotnet";
+            blog.Posts.Add(
+                new Post
+                {
+                    Title = "Hello World",
+                    Content = "I wrote an app using EF Core!"
+                });
+            db.SaveChanges();
+
+            ////уничтожение блога
+            //Console.WriteLine("Уничтожение блога");
+            //db.Remove(blog);
+            //db.SaveChanges();
+
+        }
+    }
+}
